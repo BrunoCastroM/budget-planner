76a55a83eb5f591b6567c50346e1a51470b310de
brunomoura <brunocastromoura@gmail.com>
2024-10-28 16:01:13 +0000

    Feature: Header implementation

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -9,12 +9,14 @@
 //     );
 // }
 
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 import services from '../../utils/services';
 import { client } from '../../utils/KindeConfig';
 import { supabase } from '../../utils/SupabaseConfig';
+import Header from '../../components/Header';
+import Colors from '../../utils/Colors';
 
 export default function Home() {
     const router = useRouter();
@@ -49,14 +51,28 @@ export default function Home() {
             .select('*')
             .eq('created_by', user.email);
 
-        console.log('Data:', data);
+        // console.log('Data:', data);
     };
 
     return (
-        <View style={{ marginTop: 20 }}>
-            <Text>Home</Text>
+        <View
+            style={{
+                marginTop: 30,
+                padding: 20,
+                backgroundColor: Colors.PRIMARY,
+                height: 150,
+            }}
+        >
+            <Header />
 
-            <Button title="Sair" onPress={handleLogout} />
+
+            {/* <Button title="Sair" onPress={handleLogout} /> */}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+    },
+});
