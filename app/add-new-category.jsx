8ad587ee67c2eb86041a77c84a0409dca1a083b7
brunomoura <brunocastromoura@gmail.com>
2024-10-29 21:46:36 +0000

    Feature: Finalize new category implementation

--- app/add-new-category.jsx
+++ app/add-new-category.jsx
@@ -1,11 +1,38 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ToastAndroid } from 'react-native';
 import React, { useState } from 'react';
 import Colors from '../utils/Colors';
 import ColorPicker from '../components/ColorPicker';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { supabase } from '../utils/SupabaseConfig';
+import { client } from '../utils/KindeConfig';
 
 export default function AddNewCategory() {
     const [selectedIcon, setSelectedIcon] = useState('IC');
     const [selectedColor, setSelectedColor] = useState(Colors.PURPLE);
+    const [categoryName, setCategoryName] = useState();
+    const [totalBudget, setTotalBudget] = useState();
+
+    const onCreateCategory = async () => {
+        const user = await client.getUserDetails();
+
+        const { data, error } = await supabase
+            .from('Category')
+            .insert([
+                {
+                    name: categoryName,
+                    assigned_budget: totalBudget,
+                    icon: selectedIcon,
+                    color: selectedColor,
+                    created_by: user.email,
+                },
+            ])
+            .select();
+
+        if (data) {
+            ToastAndroid.show('Categoria Criada', ToastAndroid.SHORT);
+        }
+    };
 
     return (
         <View style={{ marginTop: 20, padding: 20 }}>
@@ -28,6 +55,35 @@ export default function AddNewCategory() {
                     setSelectedColor={(color) => setSelectedColor(color)}
                 />
             </View>
+
+            <View style={styles.inputView}>
+                <MaterialIcons name="local-offer" size={24} color={Colors.GRAY} />
+                <TextInput
+                    onChangeText={(value) => setCategoryName(value)}
+                    placeholder="Nome da Categoria"
+                    style={{ width: '100%', fontSize: 18 }}
+                />
+            </View>
+
+            <View style={styles.inputView}>
+                <FontAwesome6 name="brazilian-real-sign" size={22} color={Colors.GRAY} />
+                <TextInput
+                    onChangeText={(value) => setTotalBudget(value)}
+                    placeholder="Orçamento Total"
+                    keyboardType="numeric"
+                    style={{ width: '100%', fontSize: 18 }}
+                />
+            </View>
+
+            <TouchableOpacity
+                style={styles.button}
+                disabled={!categoryName || !totalBudget}
+                onPress={() => onCreateCategory()}
+            >
+                <Text style={{ textAlign: 'center', fontSize: 18, color: Colors.WHITE }}>
+                    Salvar
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -40,5 +96,23 @@ const styles = StyleSheet.create({
         borderRadius: 99,
         paddingHorizontal: 28,
         color: Colors.WHITE,
+    },
+    inputView: {
+        borderWidth: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 6,
+        padding: 14,
+        marginTop: 20,
+        borderRadius: 10,
+        borderColor: Colors.GRAY,
+        backgroundColor: Colors.WHITE,
+    },
+    button: {
+        backgroundColor: Colors.PRIMARY,
+        padding: 15,
+        borderRadius: 10,
+        marginTop: 30,
     },
 });
